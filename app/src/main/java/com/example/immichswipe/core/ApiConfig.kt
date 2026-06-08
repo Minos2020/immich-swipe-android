b4a8d06ae8fd67d63ac7973b8af76fee5f014591
Minos67 <malo.forrest@gmail.com>
2026-06-08 16:56:53 +0200

    Gestion de session centralisée dans SessionRepository

--- app/src/main/java/com/example/immichswipe/core/ApiConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.example.immichswipe.core
-
-object ApiConfig {
-    const val BASE_URL = "https://immich.malofrst.fr/"
-
-    const val API_KEY = "9bpJBpPqVlyEaXO40ocz74TFuZlIG4PoIW4mdYiz9g"
-}
