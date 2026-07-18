e9147946728fc8d475995d68627dca48feef1e8a
Minos67 <malo.forrest@gmail.com>
2026-07-19 00:00:27 +0200

    docs: updating fastlane descriptions, versionCode and versionName for new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.minos2020.immichswipe"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0.0"
+        versionCode = 4
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
